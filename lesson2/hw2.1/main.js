37f75a2a7f11bd91a6d4e4839d886d7fbc9c6490
Vlad Bhan <61423828+Adanvege@users.noreply.github.com>
2024-08-10 00:12:15 +0300

    push

---
+++ lesson2/hw2.1/main.js
@@ -0,0 +1,68 @@
+// let arr=['cat','dog','elephant','lion','bird','fish','hedgehog','snake','tiger','animal']
+// console.log(arr)
+
+// let book = {
+//     title:'L\'Étranger',
+//     pageCount:128,
+//     genre:'tale',
+// }
+//
+// let book1 = {
+//     title:'Demian',
+//     pageCount:216,
+//     genre:'novel',
+// }
+// let book2 = {
+//     title:'Hell Screen',
+//     pageCount:224,
+//     genre:'Short story',
+// }
+//
+// let book3 = {
+//     title:'L\'Étranger',
+//     pageCount:128,
+//     genre:'tale',
+//     authors:['Albert Camus',46]
+// }
+//
+// let book4 = {
+//     title:'Demian',
+//     pageCount:216,
+//     genre:'novel',
+//     authors:['Hermann Hesse',85]
+// }
+// let book5 = {
+//     title:'Hell Screen',
+//     pageCount:224,
+//     genre:'Short story',
+//     authors:['Ryūnosuke Akutagawa',35]
+// }
+
+let userArr=[
+    {name:'Name1',userName:'userName1',password:'password1'},
+    {name:'Name2',userName:'userName2',password:'password2'},
+    {name:'Name3',userName:'userName3',password:'password3'},
+    {name:'Name4',userName:'userName4',password:'password4'},
+    {name:'Name5',userName:'userName5',password:'password5'},
+    {name:'Name6',userName:'userName6',password:'password6'},
+    {name:'Name7',userName:'userName7',password:'password7'},
+    {name:'Name8',userName:'userName8',password:'password8'},
+    {name:'Name9',userName:'userName9',password:'password9'},
+    {name:'Name10',userName:'userName10',password:'password10'},
+]
+
+for (let i=0;i<userArr.length;i++)
+{
+    console.log(userArr[i].password)
+}
+
+
+let tempDays=[
+    [25,30,19],
+    [26,29,18],
+    [25,27,16],
+    [26,26,17],
+    [28,31,15],
+    [24,32,19],
+    [26,28,18],
+]
---
+++ lesson2/hw2.2/main.js
@@ -0,0 +1,147 @@
+// let x = [prompt('x=')]
+// if (x!=0)
+// {
+//     alert('Вірно')
+// }
+// else
+// {
+//     alert('Невірно')
+// }
+//
+//
+// let time = prompt('Time =')
+// if (time<9)
+// {
+//     alert("First")
+// }
+// else if (time<29)
+// {
+//     alert("Second")
+// }
+// else if (time<49)
+// {
+//     alert("Third")
+// }
+// else if (time<59)
+// {
+//     alert("Fourth")
+// }
+// else {
+//     alert('Wrong')
+// }
+//
+//
+// let day = prompt('day =')
+// if (day<11)
+// {
+//     alert("First")
+// }
+// else if (day<21)
+// {
+//     alert("Second")
+// }
+// else if (day<31)
+// {
+//     alert("Third")
+// }
+// else {
+//     alert('Wrong')
+// }
+//
+//
+// weekDay=prompt('Today is:')
+// switch (weekDay){
+//     case 1:
+//         console.log("-Feed fish");
+//         break;
+//     case 2:
+//         console.log("-Feed cat")
+//         break
+//     case 3:
+//         console.log("-Feed dog")
+//         break
+//     case 4:
+//         console.log("-Feed owl")
+//         break
+//     case 5:
+//         console.log("-Feed fish")
+//         break
+//     case 6:
+//         console.log("-Feed cat")
+//         break
+//     case 7:
+//         console.log("-Feed dog")
+//         break
+//     default:
+//         console.log("Wrong Input")
+// }
+
+// let num1= prompt('First num = ')
+// let num2 = prompt('Second num = ')
+// if (num1>num2)
+// {
+//     console.log(num1+num2)
+// }
+// else if (num2>num1)
+// {
+//     console.log(num2+num1)
+// }
+// else {
+//     console.log(num2+num1)
+// }
+//
+// let num1= prompt('First num = ')
+// let num2 = prompt('Second num = ')
+// if (num1>num2)
+// {
+//     console.log(num1)
+// }
+// else if (num2>num1)
+// {
+//     console.log(num2)
+// }
+// else {
+//     console.log('Same')
+// }
+
+//
+// x=prompt('Enter x')
+// if (x==false)
+// {
+//     x='Default';
+//     alert(x);
+// }
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray[0].monthDuration>5)
+{
+    console.log('Super')
+}
+if (coursesAndDurationArray[1].monthDuration>5)
+{
+    console.log('Super')
+}
+if (coursesAndDurationArray[2].monthDuration>5)
+{
+    console.log('Super')
+}
+if (coursesAndDurationArray[3].monthDuration>5)
+{
+    console.log('Super')
+}
+if (coursesAndDurationArray[4].monthDuration>5)
+{
+    console.log('Super')
+}
+if (coursesAndDurationArray[5].monthDuration>5)
+{
+    console.log('Super')
+}
